1f65a89db8e1411980d1e11acb839ca1544e7231
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-02 00:51:45 -0800

    Write a manual non-assertion based test code which will require and call nextISSTimesForMyLocation() function, so that it can be visually tested

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -18,11 +18,23 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 //   console.log('It worked! Returned coord:', coord);
 // });
 
-fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, passTimes) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
+// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-  console.log('It worked! Returned flyover times:', passTimes);
+//   console.log('It worked! Returned flyover times:', passTimes);
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  for (let element of passTimes) {
+    const date = new Date(element.risetime * 1000); // Convert from seconds to milliseconds
+    const formattedDate = date.toString();// Format the date in a human-readable format
+    console.log(`Next pass at ${formattedDate} for ${element.duration} seconds!`);
+  }
 });
