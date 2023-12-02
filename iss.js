259c226ccef6a922900f1a3b0df54a870450ecc9
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-01 23:54:06 -0800

    pass through the error to the callback if an error occurs when requesting the IP data and check the HTTP status code which comes back as part of the HTTP Response(response.statusCode==200),if not 200, throw error

--- iss.js
+++ iss.js
@@ -11,7 +11,18 @@ const request = require('request');
 const fetchMyIP = function (callback) {
   // use request to fetch IP address from JSON API
   request("https://api.ipify.org/?format=json", (error, response, body) => { //url is api
-   
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
     const obj = JSON.parse(body); //to convert string into object
     const ip = obj.ip;
     callback(null, ip);
