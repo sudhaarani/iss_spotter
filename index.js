a14edef0f5924c40338ca5e9fe2853d5a93f2523
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-02 00:33:48 -0800

    Write a manual non-assertion based test code which will require and call this function, so that it can be visually tested for func fetchCoordsByIP()

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
+
+fetchCoordsByIP("75.157.124.16", (error, coord) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  console.log('It worked! Returned IP:', ip);
+  console.log('It worked! Returned coord:', coord);
 });
