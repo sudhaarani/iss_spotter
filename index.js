8c8dd6a942bca9e111a1ffd9a6bf30a09daa316f
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-02 00:35:12 -0800

    Write a manual non-assertion based test code which will require and call this function, so that it can be visually tested for func fetchISSFlyOverTimes()

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,11 +9,20 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:', ip);
 // });
 
-fetchCoordsByIP("75.157.124.16", (error, coord) => {
+// fetchCoordsByIP("75.157.124.16", (error, coord) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coord:', coord);
+// });
+
+fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  console.log('It worked! Returned coord:', coord);
+  console.log('It worked! Returned flyover times:', passTimes);
 });
