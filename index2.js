253ee52548a20a0d0f6fe0212ebb652666893498
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-12-04 11:28:55 -0800

    call the function nextISSTimesForMyLocation, call .then on its return value which should take in a callback with response body as input which contains Array of data objects defining the next fly-overs of the ISS and console log the messages to make that data more human readable

--- index2.js
+++ index2.js
@@ -1,6 +1,10 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log("body:" + body));
+nextISSTimesForMyLocation()
+  .then((response) => {
+    for (let element of response) {
+      const date = new Date(element.risetime * 1000); // Convert from seconds to milliseconds
+      const formattedDate = date.toString();// Format the date in a human-readable format
+      console.log(`Next pass at ${formattedDate} for ${element.duration} seconds!`);
+    }
+  })
